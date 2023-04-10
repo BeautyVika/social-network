66609608c3f643f8ba42d809ccf489e17e9fcff8
BeautyVika <vikachk54@gmail.com>
2023-04-10 13:44:55 +0300

    testing component 92 video

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import {Route, withRouter} from "react-router-dom";
-import Settings from "./components/Settings/Settings";
-import Music from "./components/Music/Music";
-import News from "./components/News/News";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
-import {connect} from "react-redux";
-import {compose} from "redux";
-import {AppStateType} from "redux/reduxStore";
-import {initializeApp} from "redux/AppReducer";
-import Preloader from "./components/Common/Preloader/Preloader";
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import {Route, withRouter} from "react-router-dom"
+import Settings from "./components/Settings/Settings"
+import Music from "./components/Music/Music"
+import News from "./components/News/News"
+import DialogsContainer from "./components/Dialogs/DialogsContainer"
+import UsersContainer from "./components/Users/UsersContainer"
+import ProfileContainer from "./components/Profile/ProfileContainer"
+import HeaderContainer from "./components/Header/HeaderContainer"
+import Login from "./components/Login/Login"
+import {connect} from "react-redux"
+import {compose} from "redux"
+import {AppStateType} from "redux/reduxStore"
+import {initializeApp} from "redux/AppReducer"
+import Preloader from "./components/Common/Preloader/Preloader"
 
 type MapDispatchPropsType = {
     initializeApp: () => void
@@ -40,6 +40,7 @@ class App extends React.Component<AppContainerPropsType> {
                 <HeaderContainer/>
                 {/*<Navbar/>*/}
                 <div className='app-wrapper-content'>
+                    {/*<Route path='/' render={() => <ProfileContainer/>}/>*/}
                     <Route path='/dialogs' render={() => <DialogsContainer/>}/>
                     <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
                     <Route path='/users' render={() => <UsersContainer/>}/>
