1416cb9966ae335aed16c8707fb36a8060ea8b4d
BeautyVika <vikachk54@gmail.com>
2023-04-11 11:10:36 +0300

    refactoring deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import store from "./redux/reduxStore";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom"
+import store from "./redux/reduxStore"
+import App from "./App"
 import './index.css'
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux"
+import {HashRouter} from "react-router-dom"
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
+    <Provider store={store}>
+        <HashRouter>
             <App/>
-        </Provider>
-    </BrowserRouter>, document.getElementById('root')
-);
+        </HashRouter>
+    </Provider>, document.getElementById('root')
+)
